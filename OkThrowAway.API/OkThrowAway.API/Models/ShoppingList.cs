3606231091f9cee8ebcd7fb260f006a764387697
Jordy van Essen <j.vanessen@viscon.eu>
2021-11-22 12:31:58 +0100

    Add Product and ShoppingList db models

---
+++ OkThrowAway.API/OkThrowAway.API/Models/ShoppingList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OkThrowAway.API.Models
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User User { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
