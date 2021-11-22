2539ac89589a93f05941977ae4f452ac0385be78
Jordy van Essen <j.vanessen@viscon.eu>
2021-11-22 12:24:09 +0100

    Add empty API project and database

---
+++ OkThrowAway.API/OkThrowAway.API/Models/OkThrowAwayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OkThrowAway.API.Models
+{
+    public class OkThrowAwayDbContext : DbContext
+    {
+        public OkThrowAwayDbContext(DbContextOptions<OkThrowAwayDbContext> options)
+            : base(options)
+        {
+
+        }   
+        
+        DbSet<User> Users { get; set; }
+    }
+}
