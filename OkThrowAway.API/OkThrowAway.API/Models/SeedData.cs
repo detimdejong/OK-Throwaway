045ddb657b17e90dc1528086cc02902d03e03f8d
Jordy van Essen <j.vanessen@viscon.eu>
2021-11-23 13:29:29 +0100

    Add products and ShoppingList to SeedData

--- OkThrowAway.API/OkThrowAway.API/Models/SeedData.cs
+++ OkThrowAway.API/OkThrowAway.API/Models/SeedData.cs
@@ -1,4 +1,5 @@
 ﻿using OkThrowAway.API.Helpers;
+using System.Collections.Generic;
 
 namespace OkThrowAway.API.Models
 {
@@ -7,11 +8,34 @@ namespace OkThrowAway.API.Models
         public static void Seed(OkThrowAwayDbContext db)
         {
             string salt = SecurityHelper.GenerateSalt(70);
-            db.Users.Add(new User { 
+            var defaultUser = new User { 
                 Email = "email@email.com",
                 UserName = "admin",
                 Password = SecurityHelper.HashPassword("admin", salt, 100, 70),
                 Salt = salt
+            };
+
+            db.Users.Add(defaultUser);
+
+            var products = new List<Product>()
+            {
+                new Product { Name = "A" },
+                new Product { Name = "B" },
+                new Product { Name = "C" },
+                new Product { Name = "D" },
+                new Product { Name = "E" },
+                new Product { Name = "F" },
+                new Product { Name = "G" },
+                new Product { Name = "H" },
+                new Product { Name = "I" },
+            };
+            db.AddRange(products);
+
+            db.Lists.Add(new ShoppingList
+            {
+                Name = "Supermarkt",
+                User = defaultUser,
+                Products = products
             });
 
             db.Products.Add(new Product{
