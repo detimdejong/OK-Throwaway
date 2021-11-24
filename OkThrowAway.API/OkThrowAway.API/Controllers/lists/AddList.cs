d1ba61ca4b2ea79873473502c94743d2515f3885
Tim de Jong <43207220+detimdejong@users.noreply.github.com>
2021-11-24 13:21:06 +0100

    Added endpoint for creating lists

---
+++ OkThrowAway.API/OkThrowAway.API/Controllers/lists/AddList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OkThrowAway.API.Models;
+
+namespace OkThrowAway.API.Controllers.lists
+{
+    [ApiController]
+    public class AddList : Controller
+    {
+        private readonly OkThrowAwayDbContext db;
+        private readonly IMapper mapper;
+
+        public AddList(OkThrowAwayDbContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("/api/lists/addlist")]
+        public async Task<ActionResult<ShoppingList>> PostListItem(ViewModel list)
+        {
+            var _list = mapper.Map<ShoppingList>(list);
+            _list.User = db.Users.FirstOrDefault(u => u.Id == list.UserId);
+            if (_list.User == null)
+                return BadRequest("Invalid request");
+
+            db.Lists.Add(_list);
+            await db.SaveChangesAsync();
+
+            return Ok($"Name: {_list.Name}");
+        }
+    }
+
+    public class ViewModel
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<ViewModel, ShoppingList>()
+                .ForMember(list => list.Products, o => o.MapFrom(v => new List<Product>() ));
+        }
+    }
+}
