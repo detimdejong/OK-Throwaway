27cb16e288ed72f2d06979cc4d91c1ba879a502b
Jordy van Essen <j.vanessen@viscon.eu>
2022-01-26 14:50:01 +0100

    Add deleting shoppinglists

--- okthrowaway-app/navigation/index.tsx
+++ okthrowaway-app/navigation/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Feather, FontAwesome } from '@expo/vector-icons';
+import { AntDesign, Feather, FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { ColorSchemeName, Pressable, View } from 'react-native';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import useTheme from '../hooks/useTheme';
@@ -42,6 +42,7 @@ const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
   const [visible, setVisible] = React.useState<boolean>(false);
+  const [edit, setEdit] = React.useState<boolean>(false);
   const theme = useTheme();
 
   return (
@@ -53,30 +54,61 @@ function BottomTabNavigator() {
         }}>
         <BottomTab.Screen
           name="Boodschappenlijstje"
-          component={(props) => <ListOverview {...props} reload={visible} />}
+          component={(props) => <ListOverview {...props} reload={visible} edit={edit} />}
           options={({ navigation }: RootTabScreenProps<'Boodschappenlijstje'>) => ({
             title: 'Boodschappenlijstjes',
             tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
             headerRight: () => (
-              <Pressable
-                onPress={() => setVisible(true)}
-                style={({ pressed }) => ({
-                  opacity: pressed ? 0.5 : 1,
-                })}>
-                <FontAwesome
-                  name="cart-plus"
-                  size={25}
-                  color={theme.text}
-                  style={{ marginRight: 15 }}
-                />
-              </Pressable>
+              <>
+                {edit ?
+                  <View style={{ flexDirection: "row" }}>
+                    <Pressable
+                      onPress={() => setVisible(true)}
+                      style={({ pressed }) => ({
+                        opacity: pressed ? 0.5 : 1,
+                      })}>
+                      <FontAwesome
+                        name="cart-plus"
+                        size={25}
+                        color={theme.text}
+                        style={{ marginRight: 15 }}
+                      />
+                    </Pressable>
+                    <Pressable
+                      onPress={() => setEdit(edit => !edit)}
+                      style={({ pressed }) => ({
+                        opacity: pressed ? 0.5 : 1,
+                      })}>
+                      <AntDesign
+                        name="save"
+                        size={25}
+                        color={theme.text}
+                        style={{ marginRight: 15 }}
+                      />
+                    </Pressable>
+                  </View>
+                  :
+                  <Pressable
+                    onPress={() => setEdit(edit => !edit)}
+                    style={({ pressed }) => ({
+                      opacity: pressed ? 0.5 : 1,
+                    })}>
+                    <AntDesign
+                      name="edit"
+                      size={25}
+                      color={theme.text}
+                      style={{ marginRight: 15 }}
+                    />
+                  </Pressable>
+                }
+              </>
             ),
           })}
         />
       </BottomTab.Navigator>
 
       <AddAlert visible={visible} setVisible={setVisible} />
-        
+
     </Provider>
   );
 }
